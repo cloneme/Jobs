a2c515fc0635eed0402072ac73401de51cd183b8
cloneme <cloneme>
2014-10-03 10:12:33 +0530

    permissions to update and remove collections
    permissions to update and remove collections added

--- collections/posts.js
+++ collections/posts.js
@@ -1,9 +1,40 @@
 Jobs = new Meteor.Collection('jobs');
 
 Jobs.allow({
-insert: function(userId, doc) {
-    // only allow posting if you are logged in
-return !! userId; }
+  update: ownsDocument,
+  remove: ownsDocument,
+});
+
+Jobs.deny({
+	remove: !ownsDocument
+});
+/*Jobs.deny({
+	update: function(userId, post, fields) {
+	    // may only edit the following two fields:
+	return (_.without(fields, ['organization', 'shortDescription', 'url', 'schedule', 'addedRequirements']).length > 0); }
+});*/
+
+Meteor.methods({
+	post: function(postAttributes) {
+	
+	var user = Meteor.user();
+	
+	    // ensure the user is logged in
+	if (!user)
+		throw new Meteor.Error(401, "You need to login to post new stories");
+
+	    // pick out the whitelisted keys
+	var post = _.extend(_.pick(postAttributes, 'jobTitle', 'shortDescription', 'organization', 'url', 'schedule', 'compensation', 'addedRequirements'), { 
+		userId: user._id,
+		author: user.username,
+		submitted: new Date()
+	});
+	
+	var postId = Jobs.insert(post);
+	
+		return postId; 
+	}
+
 });
 
 
