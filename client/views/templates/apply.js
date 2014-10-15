0ceab962cc0f6cd4998aa07bcf6669df82de2aa0
cloneme <cloneme>
2014-10-15 21:25:05 +0530

    base applications system
    added the base applications system

--- client/views/templates/apply.js
+++ client/views/templates/apply.js
@@ -4,3 +4,25 @@ Template.application.helpers({
 
 	},
 });
+
+Template.application.events({
+	"click #apply": function (e) {
+		
+		var jobId = Session.get('jobId');
+
+
+		var application = {
+			name: $('#name').val(),
+			about: $("#about").val(),
+			jobId: jobId
+
+		}
+		
+		alert("Hiya!");
+		application._id = Applications.insert(application);
+
+		Router.go('/jobs');
+		
+
+	},
+});
