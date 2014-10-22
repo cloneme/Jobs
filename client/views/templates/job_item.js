9c33e4855c3b7e683b71f1724972e1c1173d16d8
CLONEME <PRASANNAPEGU@GMAIL.COM>
2014-10-22 21:53:45 +0530

    edit options at job view
    user can edit their respective job posts at job-view

--- client/views/templates/job_item.js
+++ client/views/templates/job_item.js
@@ -1,6 +1,19 @@
 Template.jobItem.helpers({
+
 	ownPost: function() {
 	return this.userId == Meteor.userId(); 
 	
 	},
+
+	
+});
+
+
+Template.job.helpers({
+
+	ownPost: function() {
+	return this.userId == Meteor.userId(); 
+	
+	},
+
 });
